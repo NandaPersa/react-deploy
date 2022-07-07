240b267593e1d20c66ee8124e00ab348fd78873d
NandaPersa <anandapersa@gmail.com>
2022-07-07 19:46:47 -0300

    feat: bad clean code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [ms, setms] = useState();
+
+  useEffect(() => {
+    function calc () {
+      const res = 50 / (1.65 * 1.65);
+      console.log(res)
+
+      if (res < 18.5 ){
+        //console.log('if 1');
+        setms('Abaixo do peso');
+      }
+  
+      if (res >= 18.5 && res <= 24.9 ){
+       // console.log('if 1');
+        setms('Normal');
+      }
+      if (res >= 25 && res <= 29.9 ){
+       //console.log('if 2');
+        setms('Sobrepeso');
+      }
+  
+      if (res > 30 ){
+        //console.log('if 3');
+        setms('Obesidade');
+      }
+    }
+    calc();
+    console.log('O IMC de Maria está '+ms);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Meu primeiro deploy </h1>
+        <h1>{ms}</h1>
         <p>Uhuuulll!!</p>
         <h2> Funcionou gatinha hehe!!!</h2>
       </header>
